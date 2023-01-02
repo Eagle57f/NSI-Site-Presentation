1ab2a05bbdc9575fcd5ce21c784ee3a5238c3f38
Eagle57 <eagle57f@gmail.com>
2023-01-02 18:32:02 +0100

    t

--- Site/Site-res/load.js
+++ Site/Site-res/load.js
@@ -1,3 +1,5 @@
 if (window.location.search === "?open=true") {
     document.querySelector("nav").classList.add("is-open-no-transition");
 };
+
+alert("Hauteur " + window.innerHeight + " Largeur " + window.innerWidth);
